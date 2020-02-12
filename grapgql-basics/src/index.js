15911dec539db710352c3cc4613fd5ab3d07aff3
Daniel Cardozo C <daniel.cardozo0621@gmail.com>
2020-02-11 23:08:36 -0500

    Foreign keys configured

--- grapgql-basics/src/index.js
+++ grapgql-basics/src/index.js
@@ -1,10 +1,47 @@
 import { GraphQLServer } from 'graphql-yoga';
 
+const users = [{
+    id: '1',
+    name: 'Daniel',
+    email: 'asdasd',
+    age: 28
+},{
+    id: '2',
+    name: 'Pato',
+    email: 'asdasd2'
+},{
+    id: '3',
+    name: 'Andrés',
+    email: 'asdasd3'
+}];
+
+const posts = [{
+    id: '901',
+    title: 'My first post',
+    body: 'Basics of graphQL',
+    published: true,
+    author: '1'
+},{
+    id: '902',
+    title: 'My second post',
+    body: 'Nothing here....',
+    published: false,
+    author: '1'
+},{
+    id: '903',
+    title: 'My third post',
+    body: '',
+    published: false,
+    author: '2'
+}];
+
 // Type definitions (schema)
 const typeDefs = `
     type Query {
-        greeting(name: String, position: String): String!
+        users(query: String): [User!]!
         me: User!
+        posts(query: String): [Post!]!
+        post: Post!
     }
     
     type User {
@@ -12,18 +49,29 @@ const typeDefs = `
         name: String!
         email: String!
         age: Int
+        posts: [Post!]!
+    }
+    
+    type Post {
+        id: ID!
+        title: String!
+        body: String!
+        published: Boolean!
+        author: User!
     }
 `;
 
 // Resolvers
 const resolvers = {
     Query: {
-        greeting(parent, args, ctx, info) {
-            if (args.name && args.position) {
-                return `Hello, ${args.name}! you are my favourite ${args.position}`;
-            } else {
-                return 'Hello!'
+        users(parent, args, ctx, info) {
+            if (!args.query) {
+                return users;
             }
+
+            return users.filter((user) => {
+                return user.name.toLowerCase().includes(args.query.toLowerCase());
+            });
         },
         me() {
             return {
@@ -32,6 +80,39 @@ const resolvers = {
                 email: 'asdasd',
                 age: 28
             }
+        },
+        posts(parent, args, ctx, info) {
+            if (!args.query) {
+                return posts;
+            }
+
+            return posts.filter((post) => {
+                const isTitleMatch = post.title.toLowerCase().includes(args.query.toLowerCase());
+                const isBodyMatch = post.body.toLowerCase().includes(args.query.toLowerCase());
+                return isTitleMatch || isBodyMatch;
+            });
+        },
+        post() {
+            return {
+                id: '882',
+                title: 'My first post',
+                body: '',
+                published: false
+            }
+        }
+    },
+    Post: {
+        author(parent, args, ctx, info) {
+            return users.find((user) => {
+                return user.id === parent.author
+            })
+        }
+    },
+    User: {
+        posts(parent, args, ctx, info) {
+            return posts.filter((post) => {
+                return post.author === parent.id
+            })
         }
     }
 };
